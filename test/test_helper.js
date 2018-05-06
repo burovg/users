6351b7514fed03218e854d6e6f7b19ef1847860e
burovg <burovg@gmail.com>
2018-05-06 16:32:54 +0300

    new one

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+mongoose.connect('mongodb://localhost:27017/users_test');
 mongoose.connection
 .once('open',() => {
     console.log('Good to go');
